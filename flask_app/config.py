031e0281f3c2d6c57bae68d16ee383f71e87f793
tomowarkar <3104kawamoto@gmail.com>
2019-10-20 00:09:21 +0900

    del env var

--- flask_app/config.py
+++ flask_app/config.py
@@ -1,6 +1,6 @@
-URL = "https://tomowarkar.com"
+URL = ""
 
 # /api/koubun.py
-KUSER = 'apikey'
-KPSW = 'M1B7dStQw49nyso4bFPfkEFMGPasiWiYUY3asQOwTT8M'
-KURL = 'https://gateway-tok.watsonplatform.net/natural-language-understanding/api/v1/analyze?version=2018-11-16'
+KUSER = ''
+KPSW = ''
+KURL = ''
