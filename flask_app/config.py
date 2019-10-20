fdf17b6d0c6e832946a090af33041a5213300d7a
tomowarkar <3104kawamoto@gmail.com>
2019-10-20 13:49:34 +0900

    add git

--- flask_app/config.py
+++ flask_app/config.py
@@ -1,15 +1,15 @@
 # master branchでは記入しない
 # heroku branchで記入
 
-URL = "https://tomowarkar.com"
+URL = ""
 
 # /api/koubun.py
-KUSER = 'apikey'
-KPSW = 'M1B7dStQw49nyso4bFPfkEFMGPasiWiYUY3asQOwTT8M'
-KURL = 'https://gateway-tok.watsonplatform.net/natural-language-understanding/api/v1/analyze?version=2018-11-16'
+KUSER = ''
+KPSW = ''
+KURL = ''
 
-LINE_CHANNEL_ACCESS_TOKEN = '14Z2iMlxhEFnbHG6OCztbZzik/RorvaJQLr7PHtbsX80k/LIPnLY5FMQg07fl4eJ2HUlxocGRT6BfG4KLSfdJuSv/q6rUh+tWcp4lu+viV61UTXxpBgKgrNob2N+7Q4a5aCX4ybWqB936ONEpBN1RQdB04t89/1O/w1cDnyilFU='
-LINE_CHANNEL_SECRET = 'b390154dc5c0f753be9203ca182c0b68'
+LINE_CHANNEL_ACCESS_TOKEN = ''
+LINE_CHANNEL_SECRET = ''
 
-MKEY = "AIzaSyD-BpqpUoaruYTmqbwiFFjfXeWktdlhEPg"
-MURL = 'https://vision.googleapis.com/v1/images:annotate?key='
+MKEY = ""
+MURL = ''
